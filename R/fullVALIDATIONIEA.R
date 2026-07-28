497baaeefc920320db89458b174311c71b52ce6d
Falk Benke <falk.benke@pik-potsdam.de>
2026-07-28 18:07:22 +0200

    update test

--- R/fullVALIDATIONIEA.R
+++ R/fullVALIDATIONIEA.R
@@ -6,20 +6,7 @@ fullVALIDATIONIEA <- function() {
 
   # Industry ----
 
-  gdpPopScen <- c("SSPs", "SSP2IndiaDEAs")
-  feDemScen <- c(gdpPopScen, "SSP2_lowEn", "SSP2_highDemDEU", "SSP2_NAV_all")
-
   calcOutput("EnergyBalancesOutputToIndustry", file = "EnergyBalancesOutputToIndustry.cs4r")
-
-  calcOutput("FeDemandIndustry", scenarios = feDemScen, signif = 4, last_empirical_year = 2022,
-             file = "f_fedemandInd_2022.cs4r")
-
-  calcOutput("FeDemandIndustry",
-             scenarios = "SSP2",
-             signif = 4,
-             aggregate = FALSE,
-             file = "f_fedemandInd_unaggregated.cs4r")
-
 
   # Historical ----
 
@@ -27,7 +14,6 @@ fullVALIDATIONIEA <- function() {
   calcOutput(type = "FE", ieaVersion = "latest", file = "FE.cs4r")
 
   # Buildings ----
-
   calcOutput(type = "IOEdgeBuildings", ieaVersion = "default",
              subtype = "output_EDGE", file = "IOEdgeBuildings_output_EDGE.cs4r")
   calcOutput(type = "IOEdgeBuildings", ieaVersion = "default",
